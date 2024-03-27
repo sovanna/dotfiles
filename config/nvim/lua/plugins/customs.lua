3cc41687ef2f862b87480f10c0a1ca44a81a4cd5
Sovanna <sovanna.hing@gmail.com>
2024-03-27 16:33:43 +0100

    refactor(nvim): Update

--- config/nvim/lua/plugins/customs.lua
+++ config/nvim/lua/plugins/customs.lua
@@ -1,23 +1,18 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    version = false,
   },
   {
     "L3MON4D3/LuaSnip",
-    version = false,
   },
   {
     "nvim-telescope/telescope.nvim",
-    version = false,
   },
   {
     "nvim-neo-tree/neo-tree.nvim",
-    version = false,
   },
   {
     "linux-cultist/venv-selector.nvim",
-    version = false,
     opts = {
       -- Your options go here
       -- name = "venv",
@@ -28,18 +23,20 @@ return {
   },
   {
     "folke/which-key.nvim",
-    version = false,
   },
   {
     "RRethy/vim-illuminate",
-    version = false,
   },
   {
     "akinsho/bufferline.nvim",
-    version = false,
   },
   {
     "mg979/vim-visual-multi",
-    branch = "master",
   },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^4", -- Recommended
+    ft = { "rust" },
+  },
+  { "nvim-neotest/nvim-nio" },
 }
